7ed27179d4faa8265b9a7911239bd6914c048c4b
Raiyad Raad <raiyad.raad@gmail..com>
2025-05-02 14:00:56 +0200

    gitignore file updated, other files added for anchor build steps

---
+++ programs/my_solana_project/src/lib.rs
@@ -0,0 +1,39 @@
+use anchor_lang::prelude::*;
+
+declare_id!("CSfhQDbtBf7qtBr4qgnSrbGxLB3LU1PSYBcLyEcCh3Zj"); // Replace with your actual program ID later
+
+#[program]
+pub mod my_solana_project {
+    use super::*;
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
+}
