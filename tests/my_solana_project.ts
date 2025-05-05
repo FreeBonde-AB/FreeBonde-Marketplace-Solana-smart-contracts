f72c14d3f1a71017efa7251fb300b651476f8f14
Raiyad Raad <raiyad.raad@gmail..com>
2025-05-05 09:45:36 +0200

    gitignore updated

--- tests/my_solana_project.ts
+++ tests/my_solana_project.ts
@@ -1,32 +1,13 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { MySolanaProject } from "../target/types/my_solana_project";
-import { assert } from "chai";
 
 describe("my_solana_project", () => {
-  const provider = anchor.AnchorProvider.env();
-  anchor.setProvider(provider);
+  // Configure the client to use the devnet cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.MySolanaProject as Program<MySolanaProject>;
+  const program = anchor.workspace.MySolanaProject;
 
-  it("Initializes and increments!", async () => {
-    const counter = anchor.web3.Keypair.generate();
-
-    await program.methods
-      .initialize()
-      .accounts({
-        counter: counter.publicKey,
-        user: provider.wallet.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-      })
-      .signers([counter])
-      .rpc();
-
-    await program.methods.increment()
-      .accounts({ counter: counter.publicKey })
-      .rpc();
-
-    const account = await program.account.counter.fetch(counter.publicKey);
-    assert.equal(account.count.toNumber(), 1);
+  it("Is initialized!", async () => {
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
   });
 });
